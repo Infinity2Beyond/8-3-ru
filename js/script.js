a51f0c1d0023ebffb3c6583eac42f92c14747503
Infinity2Beyond <92722335+Infinity2Beyond@users.noreply.github.com>
2024-03-08 01:40:41 +0300

    push
    8-3 ru lang

---
+++ js/script.js
@@ -0,0 +1,142 @@
+
+function hide() {
+    const x = document.getElementById("but3");
+    const y = document.getElementById("but2");
+     x.style.display="block";
+     y.style.display="none";    
+}
+function hide2() {
+    const x = document.getElementById("but3");
+    const y = document.getElementById("but4");
+     x.style.display="none";
+     y.style.display="block";
+}
+function hide3() {
+    const x = document.getElementById("but4");
+    const y = document.getElementById("but2");
+     x.style.display="none";
+     y.style.display="none";
+}
+function show1() {
+    const x = document.getElementById("click");
+    const y = document.getElementById("first");
+     x.style.display="none";
+     y.style.display="block"; 
+     document.getElementById("audio3").play();
+}
+function show2() {
+    const x = document.getElementById("yess");
+    const y = document.getElementById("boxx1");
+     x.style.display="none";
+     y.style.display="block"; 
+}
+function show3() {
+    const x = document.getElementById("boxx1");
+    const y = document.getElementById("img12A1");
+    x.style.display="none";
+    y.style.display="block"; 
+}
+function show4() {
+    const x = document.getElementById("img12A1");
+    const y = document.getElementById("second");
+    x.style.display="none";
+    y.style.display="block"; 
+}
+function show5() {
+    const x = document.getElementById("second");
+    const y = document.getElementById("third");
+    x.style.display="none";
+    y.style.display="block"; 
+}
+function backtoending() {
+    const x = document.getElementById("img12A1");
+    const y = document.getElementById("yess");
+    x.style.display="none";
+    y.style.display="block"; 
+}
+function realending() {
+    const x = document.getElementById("third");
+    const y = document.getElementById("end");
+    x.style.display="none";
+    y.style.display="block"; 
+}
+function show() {
+    const x = document.getElementById("first");
+    const y = document.getElementById("form1");
+    const a = document.getElementById("but3");
+    const b = document.getElementById("but4");
+    a.style.display="none";
+    b.style.display="none";
+    x.style.display="none";
+    y.style.display="block";  
+}
+function troll() {
+    const x = document.getElementById("fbut2");
+    const y = document.getElementById("fbut3");
+      x.style.display="none";
+      y.style.display="block";
+      document.getElementById("alert").innerHTML = "😒";
+      document.getElementById("alert2").innerHTML = "Cậu mà tắt hay thoát là mình bùn lắm ó!";
+      return false;
+}
+function troll1() {
+    const x = document.getElementById("fbut3");
+    const y = document.getElementById("fbut4");
+     x.style.display="none";
+     y.style.display="block";
+     return false;
+}
+function troll2() {
+    const x = document.getElementById("fbut2");
+    const y = document.getElementById("fbut4");
+     x.style.display="none";
+     y.style.display="none";
+     return false;
+}
+function inrel() {
+    const x = document.getElementById("fbut1alt");
+    const y = document.getElementById("fbut1");
+    const z = document.getElementById("click");
+    const c = document.getElementById("h23");
+    const d = document.getElementById("h24");
+    const v =  document.getElementById("fbut2");
+    const b = document.getElementById("fbut2alt");
+     b.style.display="none";
+     z.style.display="none";
+     y.style.display="none";
+     c.style.display="none";
+     d.style.display="block";
+     v.style.display="inline";
+     x.style.display="inline";
+     document.getElementById("alert2").innerHTML = "";
+     return false;
+}
+function inrel2() {
+    document.getElementById("click").style.display="none";
+    document.getElementById("yess").style.display="block";
+    document.getElementById("form1").style.display="none";
+    document.getElementById("audio3").pause();
+    document.getElementById("audio").play();
+    return false;
+}
+function trollalt() {
+    document.getElementById("alert2").innerHTML="Sao cậu hong muốn, sẽ rất bất ngờ đấy!";
+    alert("Sao cậu hong muốn, sẽ rất bất ngờ đấy!");
+    return false;
+}
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    
+    heart.innerText = '💗';
+    
+    document.body.appendChild(heart);
+    
+    setTimeout(() => {
+                heart.remove();
+                }, 5000);
+  }
